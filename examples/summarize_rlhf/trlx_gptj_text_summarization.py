968b79f394a88d27b2395702d7f442237d1a0b01
Carro <robertmyers@hey.com>
2023-01-18 18:05:38 -0600

    changes

--- examples/summarize_rlhf/trlx_gptj_text_summarization.py
+++ examples/summarize_rlhf/trlx_gptj_text_summarization.py
@@ -116,13 +116,12 @@ if __name__ == "__main__":
 
     dataset = load_dataset("Dahoas/sft-synthetic-hh")
 
-    # use only the first 100 samples
-    dataset = dataset["train"][:100]
-    
-    import code; code.interact(local=dict(globals(), **locals()))
+
+
 
     # Store data into prompt and label pairs
-    train_set = [(sample["prompt"], sample["response"]) for sample in dataset]
+    train_set = [(sample["prompt"], sample["response"]) for sample in dataset['train']]
+    train_set = train_set[:1000]
     # train_set = [(sample["prompt"], sample["response"]) for sample in dataset]
     # Split into train and validation sets
     train_set, val_set = train_test_split(train_set, test_size=0.1, random_state=42)
