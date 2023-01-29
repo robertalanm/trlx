d7fdd704c0f4011f9f8a75ee8eb5ed0932c7e0aa
Carro <robertmyers@hey.com>
2023-01-29 13:12:31 -0600

    tweaked the generation parameters

---
+++ examples/summarize_rlhf/triton_tests.py
@@ -0,0 +1,35 @@
+import os
+
+import numpy as np
+
+from tritonclient.utils import np_to_triton_dtype
+import tritonclient.grpc as client_util
+
+from transformers import AutoTokenizer
+
+def prepare_tensor(name: str, input):
+    t = client_util.InferInput(name, input.shape, np_to_triton_dtype(input.dtype))
+    t.set_data_from_numpy(input)
+    return t
+
+
+reward_tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
+
+triton_host = os.environ.get("TRITON_HOST", "localhost:8001")
+triton_model = os.environ.get("TRITON_MODEL", "gpt-j")
+
+client = client_util.InferenceServerClient(url=triton_host, verbose=False)
+
+input = reward_tokenizer("Assistant: Hello! How are you today", padding=True, max_length=1024)
+input_ids = np.array(input.input_ids, dtype=np.int32)
+attention_mask = np.array(input.attention_mask, dtype=np.int8)
+
+inputs = [
+    prepare_tensor("input_ids", input_ids),
+    prepare_tensor("attention_mask", attention_mask),
+]
+
+result = client.infer(triton_model, inputs)
+rewards = result.as_numpy("rewards")
+print(rewards)
+
