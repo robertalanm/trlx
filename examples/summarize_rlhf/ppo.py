5d20685e5ab4f5a3c074ee5d76fed9c53ffd13ae
Carro <robertmyers@hey.com>
2023-01-30 19:57:51 -0600

    added SFT and RM

--- examples/summarize_rlhf/ppo.py
+++ examples/summarize_rlhf/ppo.py
@@ -4,6 +4,9 @@ import re
 
 import numpy as np
 import torch
+
+from tqdm import tqdm
+
 import tritonclient.grpc as client_util
 import yaml
 from datasets import load_dataset
@@ -20,6 +23,15 @@ triton_host = os.environ.get("TRITON_HOST", "localhost:8001")
 triton_model = os.environ.get("TRITON_MODEL", "gptj-rm-static")
 
 
+def train_test_split(data, test_size=0.1, random_state=42):
+    import numpy as np
+
+    np.random.seed(random_state)
+    np.random.shuffle(data)
+    split_index = int(len(data) * (1 - test_size))
+    return data[:split_index], data[split_index:]
+
+
 def prepare_tensor(name: str, input):
     t = client_util.InferInput(name, input.shape, np_to_triton_dtype(input.dtype))
     t.set_data_from_numpy(input)
@@ -32,6 +44,12 @@ def main(hparams={}):
     reward_tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
     reward_tokenizer.pad_token = reward_tokenizer.eos_token
     reward_tokenizer.truncation_side = "left"
+
+    max_length_input = (
+        config.train.seq_length - config.method.gen_kwargs["max_new_tokens"]
+    )
+
+
     client = client_util.InferenceServerClient(url=triton_host, verbose=False)
 
     def reward_fn(samples, prompts, outputs):
@@ -62,18 +80,71 @@ def main(hparams={}):
         print('out', out)
         return out
 
-    def preprocess(sample):
-        sample["prompt"] += "Assistant:"
-        return sample
+    def get_prompt_dataset(prompts, max_length):
+        """
+        Get the prompt after T5 decoding to make sure dictionary
+        of prompts and summaries is consistent decode prompt from trlX pipeline
+        """
+        formatted_prompts = []
+        for i in tqdm(range(len(prompts))):
+            tmp = reward_tokenizer.decode(
+                reward_tokenizer(
+                    prompts[i].split("Assistant:")[0],
+                    truncation=True,
+                    max_length=max_length
+                    - 11,  # to make sure "TL;DR" dont get truncated
+                )["input_ids"],
+                skip_special_tokens=True,
+            ).strip()
+            tmp = tmp + "\nAssistant:"
+            tmp = reward_tokenizer.decode(
+                reward_tokenizer(tmp, truncation=True, max_length=max_length)["input_ids"],
+                skip_special_tokens=True,
+            ).strip()
+            formatted_prompts.append(tmp)
+        return formatted_prompts
 
-    dataset = load_dataset("Dahoas/rm-static").map(preprocess)
-    prompts = dataset["train"]["prompt"]
-    eval_prompts = dataset["test"]["prompt"][:8]
+
+    dataset = load_dataset("Dahoas/rm-static")
+    # prompts = dataset["train"]["prompt"]
+    # eval_prompts = dataset["test"]["prompt"][:8]
+
+    # dataset = load_dataset("Dahoas/rm-synthetic-hh")
+
+    # Store data into prompt and label pairs
+    train_set = [(sample["prompt"], sample["response"]) for sample in dataset['train']]
+
+
+    # train_set = [(sample["prompt"], sample["response"]) for sample in dataset]
+    # Split into train and validation sets
+    train_set, val_set = train_test_split(train_set, test_size=0.1, random_state=42)
+
+    # shuffle val set, train set
+    import random
+    random.shuffle(train_set)
+    random.shuffle(val_set)
+
+    # train_set = train_set[:100]
+    # Split contents into summaries and labels
+    train_posts, train_summaries = zip(*train_set)
+    val_posts, val_summaries = zip(*val_set)
+
+    # shuffle val set, train set
+    # Get the OpenAI summaries
+    post_summary_dict = {}
+    train_prompts = get_prompt_dataset(train_posts, max_length_input)
+    for i in range(len(train_prompts)):
+        post_summary_dict[train_prompts[i]] = train_summaries[i]
+    val_prompts = get_prompt_dataset(val_posts, max_length_input)
+    for i in range(len(val_prompts)):
+        post_summary_dict[val_prompts[i]] = val_summaries[i]
 
     trainer = trlx.train(
         reward_fn=reward_fn,
-        prompts=prompts,
-        eval_prompts=eval_prompts,
+        prompts=train_prompts,
+        eval_prompts=val_prompts[
+            0:10
+        ],
         config=config,
         stop_sequences=["Human:", "human:", "Assistant:", "assistant:"],
     )
