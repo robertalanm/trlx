b255f2cfc4898d47f377030b1259d6290d381c66
Carro <robertmyers@hey.com>
2023-01-31 14:39:41 -0600

    added SFT and RM

--- examples/summarize_rlhf/ppo_tests.py
+++ examples/summarize_rlhf/ppo_tests.py
@@ -5,24 +5,23 @@ import os
 
 
 
-def generate(text, prompt):
+def generate(text):
     
-    prompt = prompt + f"Human: {text}\nAssistant:"
+    prompt = f"Human: {text}\nAssistant:"
 
-    response = pipe(text, max_length=1024, min_length=32, do_sample=True, top_k=512, top_p=0.92, num_return_sequences=1,)[0]['generated_text']
+    response = pipe(prompt, max_length=1024, min_length=32, do_sample=True, top_k=512, top_p=0.92, num_return_sequences=1,)[0]['generated_text']
 
-    prompt = prompt + response + "\n"
 
-    return response, prompt
+    return response
 
 # generate("Hello! How are you today?")
 
 # build a chatbot cli
 
-def run_chatbot(prompt):
+def run_chatbot():
     while True:
         text = input("HUMAN> ")
-        response, prompt = generate(text, prompt)
+        response = generate(text)
         print(f"SYBIL> {response}")
 
 if __name__ == "__main__":
@@ -37,12 +36,10 @@ if __name__ == "__main__":
     fp32_model = load_state_dict_from_zero_checkpoint(model, os.path.join(model_path))
 
 
-    prompt = ""
-
     pipe = pipeline(
         "text-generation",
         model=model,
         tokenizer=tokenizer,
         device=1,
     )
-    run_chatbot(prompt)
+    run_chatbot()
